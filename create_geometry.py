3a8ae415948e666b28db0613786e70cd48f641aa
Sam Lam <trykslam@gmail.com>
2021-05-01 16:23:58 +0200

    Update expected attribute dimensions
    Expected attribute dimensions are wrong. There are
    n_blades, each blade is discretized so that there are
    n_span control points. Each control point has n_theta
    filament (coordinates).
    
    Blade panels have four dimensions, there are n_span
    panels and there are n_blades.
    
    Also n_span attribute is added.

--- create_geometry.py
+++ create_geometry.py
@@ -14,19 +14,23 @@ class BladeGeometry:
         self.tsr = tsr
         self.v_inf = v_inf
         self.n_blades = n_blades
+        self.n_span = n_span
         self.n_theta = n_theta
         self.span_arr = np.linspace(0.2, 1.0, n_span)
         self.theta_arr = np.linspace(0, 2 * np.pi, n_theta)
 
-        self.cp = np.zeros((3, n_span, 3))  # coord; normal; tangential
-        self.rings = {"x1": np.zeros((n_theta, n_theta, n_blades)),
-                      "x2": np.zeros((n_theta, n_theta, n_blades)),
-                      "y1": np.zeros((n_theta, n_theta, n_blades)),
-                      "y2": np.zeros((n_theta, n_theta, n_blades)),
-                      "z1": np.zeros((n_theta, n_theta, n_blades)),
-                      "z2": np.zeros((n_theta, n_theta, n_blades))
+        self.cp = np.zeros((3, n_span, 9))  # coord; normal; tangential
+        self.rings = {"x1": np.zeros((n_blades, n_span, 2*n_theta+2)),
+                      "x2": np.zeros((n_blades, n_span, 2*n_theta+2)),
+                      "y1": np.zeros((n_blades, n_span, 2*n_theta+2)),
+                      "y2": np.zeros((n_blades, n_span, 2*n_theta+2)),
+                      "z1": np.zeros((n_blades, n_span, 2*n_theta+2)),
+                      "z2": np.zeros((n_blades, n_span, 2*n_theta+2)),
+                      "gamma": np.zeros((n_blades, n_span, 2*n_theta+2))
                       }
-        self.bladepanels = {}  # empty dict to store blade geometry
+        self.bladepanels = np.zeros((n_blades, n_span, 4*3))  # empty dict to
+        # store
+        # blade geometry
 
     def discretize_blade(self):
         # TODO: solve control points
