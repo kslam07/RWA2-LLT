25ea5ac0de7adf6a2312c1aac32d91ae197c52c6
Sam Lam <trykslam@gmail.com>
2021-05-05 00:18:28 +0200

    Add script file and airfoil data in csv file
    Note: liftling_line_solver is not runnable yet due to two known errors.
    1. _velocity_3D_from_vortex_filament does not correctly consider
    effect of all filaments on a single control point.
    2. u_azim not correctly computed via dot product.

---
+++ main.py
@@ -0,0 +1,7 @@
+from create_geometry import BladeGeometry
+from liftling_line_solver import LiftingLineSolver
+
+prop_geo = BladeGeometry(radius=1.0, tsr=8, v_inf=10.0, n_blades=3, n_span=5, n_theta=5)
+solver = LiftingLineSolver(geo=prop_geo, u_rot=10/50, r_rotor=50, tol=1e-3, n_iter=100)
+
+solver.run_solver()
