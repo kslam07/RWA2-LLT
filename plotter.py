5c2129f70281d78ef0983f0f02d9825bc7ba3b97
Sam Lam <trykslam@gmail.com>
2021-05-08 12:18:44 +0200

    Combine wake, blade, and hull in one plot

--- plotter.py
+++ plotter.py
@@ -66,61 +66,56 @@ import random
 # ax = Axes3D(fig)
 
 
-# ax.scatter(rings[0,:24,1:26],rings[1,:24,1:26],rings[2,:24,1:26])
-# ax.scatter(rings[3,:24,1:26],rings[4,:24,1:26],rings[5,:24,1:26])
-# ax.scatter(rings[0,24:48],rings[1,:24],rings[2,:24])
-# ax.scatter(rings[0,24:48],rings[1,:24],rings[2,:24])
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlaebl('z')
-# plt.show()
-
-############################################### PLOT BLADE PANELS ######################################################
-# fig = plt.figure(dpi=150)
-# ax1 = fig.add_subplot(131, projection="3d")
+# =============================================================================
+# BLADE VIZ
+# =============================================================================
+fig = plt.figure(dpi=150)
+ax1 = fig.add_subplot(111, projection="3d")
 # ax2 = fig.add_subplot(132, projection="3d")
 # ax3 = fig.add_subplot(133, projection="3d")
 # axes = [ax1, ax2, ax3]
 
 # split blade panels
-# blades_split = np.split(blade, 3)
+blades_split = np.split(blade, 3)
 
-# for blade_i, ax in zip(blades_split, axes):
-#     x_le = blade_i[:, 0]
-#     y_le = blade_i[:, 1]
-#     z_le = blade_i[:, 2]
-#     x_te = blade_i[:, 9]
-#     y_te = blade_i[:, 10]
-#     z_te = blade_i[:, 11]
-#     x = np.vstack((x_le, x_te))
-#     y = np.vstack((y_le, y_te))
-#     z = np.vstack((z_le, z_te))
-#     ax.view_init(10, 180)
-#     ax.set_xlabel("x")
-#     ax.set_ylabel("y")
-#     ax.set_zlabel("z")
-#     ax.plot_wireframe(x, y, z)
-# ax1.plot(solver.filaments[0,0,:],solver.filaments[1,0,:],solver.filaments[2,0,:])
+for blade_i in (blades_split):
+    x = blade_i[:, [0, 9]]
+    y = blade_i[:, [1, 10]]
+    z = blade_i[:, [2, 11]]
+
+    ax1.plot_surface(x, y, z, color='k')
 
 # =============================================================================
-# plotjes
+# Draw rotor hub
+# =============================================================================
+theta = np.linspace(0, 2 * np.pi)
+y = solver.geo.span_arr[0]*np.cos(theta)*solver.r_rotor
+z = solver.geo.span_arr[0]*np.sin(theta)*solver.r_rotor
+
+ax1.plot(y*0, y, z, color='k')
+
+# =============================================================================
+# WAKE VIZ
 # =============================================================================
 
-# fig = plt.figure()
-# ax = Axes3D(fig)
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlabel('z')
+ax1.plot_wireframe(rings[0, :nspan-1, :ntheta+1],
+                  rings[1, :nspan-1, :ntheta+1],
+                  rings[2, :nspan-1, :ntheta+1],
+                  color='green', cstride=0)
+ax1.plot_wireframe(rings[0, nspan-1:2*nspan-2, :ntheta+1],
+                  rings[1, nspan-1:2*nspan-2, :ntheta+1],
+                  rings[2, nspan-1:2*nspan-2, :ntheta+1],
+                  color='blue', cstride=0)
+ax1.plot_wireframe(rings[0, (2*nspan-2):, :ntheta+1],
+                  rings[1, (2*nspan-2):, :ntheta+1],
+                  rings[2, (2*nspan-2):, :ntheta+1],
+                  color='red', cstride=0)
 
-# ax.plot_wireframe(rings[0, :nspan-1, :ntheta+1],
-#                   rings[1, :nspan-1, :ntheta+1],
-#                   rings[2, :nspan-1, :ntheta+1],
-#                   color='green', cstride=0)
-# ax.plot_wireframe(rings[0, nspan-1:2*nspan-2, :ntheta+1],
-#                   rings[1, nspan-1:2*nspan-2, :ntheta+1],
-#                   rings[2, nspan-1:2*nspan-2, :ntheta+1],
-#                   color='blue', cstride=0)
-# ax.plot_wireframe(rings[0, (2*nspan-2):, :ntheta+1],
-#                   rings[1, (2*nspan-2):, :ntheta+1],
-#                   rings[2, (2*nspan-2):, :ntheta+1],
-#                   color='red', cstride=0)
+ax1.view_init(0, 180)
+ax1.set_xlabel("x")
+ax1.set_ylabel("y")
+ax1.set_zlabel("z")
+ax1.set_xlim(-5, 5)
+ax1.set_ylim(-50, 50)
+ax1.set_zlim(-50, 50)
+ax1.axis("equal")
