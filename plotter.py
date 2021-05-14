1707eb1e1dbf1a341fb14eb154e8f280003365c5
Sam Lam <trykslam@gmail.com>
2021-05-14 13:10:24 +0200

    Reformat script files

--- plotter.py
+++ plotter.py
@@ -21,18 +21,20 @@ rings = prop_geo.filaments
 solver = LiftingLineSolver(geo=prop_geo, r_rotor=50, weight=0.5, tol=1e-6, n_iter=500, double_rotor=False)
 
 data = solver.run_solver()
-omega = solver.geo.tsr*solver.geo.v_inf/solver.geo.radius
+omega = solver.geo.tsr * solver.geo.v_inf / solver.geo.radius
 [CP_LLM, CT_LLM] = solver.CP_and_CT(np.resize(data[0], data[2].shape), np.resize(data[1], data[2].shape), data[2],
                                     np.resize(data[3], data[2].shape), np.resize(data[4], data[2].shape),
                                     solver.geo.v_inf, omega, solver.geo.radius, nblades)
 
+
 # =============================================================================
 # Rotor performance plots
 # =============================================================================
-def plottingFunction(prop_geo,data):
-    plt.plot(prop_geo.centerPoints,data[-1][:len(prop_geo.centerPoints)])
+def plottingFunction(prop_geo, data):
+    plt.plot(prop_geo.centerPoints, data[-1][:len(prop_geo.centerPoints)])
     plt.show()
     return
+
 
 # plottingFunction(solver,prop_geo,data)
 
@@ -65,27 +67,27 @@ for blade_i in (blades_split):
 # Draw rotor hub
 # =============================================================================
 theta = np.linspace(0, 2 * np.pi)
-y = solver.geo.span_arr[0]*np.cos(theta)*solver.r_rotor
-z = solver.geo.span_arr[0]*np.sin(theta)*solver.r_rotor
+y = solver.geo.span_arr[0] * np.cos(theta) * solver.r_rotor
+z = solver.geo.span_arr[0] * np.sin(theta) * solver.r_rotor
 
-ax1.plot(y*0, y, z, color='k')
+ax1.plot(y * 0, y, z, color='k')
 
 # =============================================================================
 # WAKE VIZ
 # =============================================================================
 
-ax1.plot_wireframe(rings[0, :nspan-1, :ntheta+1],
-                  rings[1, :nspan-1, :ntheta+1],
-                  rings[2, :nspan-1, :ntheta+1],
-                  color='green', cstride=0)
-ax1.plot_wireframe(rings[0, nspan-1:2*nspan-2, :ntheta+1],
-                  rings[1, nspan-1:2*nspan-2, :ntheta+1],
-                  rings[2, nspan-1:2*nspan-2, :ntheta+1],
-                  color='blue', cstride=0)
-ax1.plot_wireframe(rings[0, (2*nspan-2):, :ntheta+1],
-                  rings[1, (2*nspan-2):, :ntheta+1],
-                  rings[2, (2*nspan-2):, :ntheta+1],
-                  color='red', cstride=0)
+ax1.plot_wireframe(rings[0, :nspan - 1, :ntheta + 1],
+                   rings[1, :nspan - 1, :ntheta + 1],
+                   rings[2, :nspan - 1, :ntheta + 1],
+                   color='green', cstride=0)
+ax1.plot_wireframe(rings[0, nspan - 1:2 * nspan - 2, :ntheta + 1],
+                   rings[1, nspan - 1:2 * nspan - 2, :ntheta + 1],
+                   rings[2, nspan - 1:2 * nspan - 2, :ntheta + 1],
+                   color='blue', cstride=0)
+ax1.plot_wireframe(rings[0, (2 * nspan - 2):, :ntheta + 1],
+                   rings[1, (2 * nspan - 2):, :ntheta + 1],
+                   rings[2, (2 * nspan - 2):, :ntheta + 1],
+                   color='red', cstride=0)
 
 ax1.view_init(0, 180)
 ax1.set_xlabel("x")
@@ -101,15 +103,17 @@ ax1.axis("auto")
 # =============================================================================
 plt.close('All')
 
-[BEM_rR, BEM_alpha, BEM_phi, BEM_rho, BEM_Ax, BEM_Az, BEM_Gamma , BEM_CT, BEM_CP, BEM_a, BEM_aline] = read_matlab_data()
+[BEM_rR, BEM_alpha, BEM_phi, BEM_rho, BEM_Ax, BEM_Az, BEM_Gamma, BEM_CT, BEM_CP, BEM_a, BEM_aline] = read_matlab_data()
 
 # Radial distribution alpha and phi
 
 plt.figure()
-plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_alpha, 0), BEM_rR.shape)[0, :]*180/np.pi, '-r', label=r'$\alpha$ BEM')
-plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_phi, 0), BEM_rR.shape)[0, :]*180/np.pi, '-b', label=r'$\phi$ BEM')
-plt.plot(data[2][:nspan-1, 0], np.degrees(np.resize(data[6], data[2].shape)[:nspan-1, 0]), '--r', label=r'$\alpha$ LLM')
-plt.plot(data[2][:nspan-1, 0], np.degrees(np.resize(data[7], data[2].shape)[:nspan-1, 0]), '--b', label=r'$\phi$ LLM')
+plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_alpha, 0), BEM_rR.shape)[0, :] * 180 / np.pi, '-r', label=r'$\alpha$ BEM')
+plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_phi, 0), BEM_rR.shape)[0, :] * 180 / np.pi, '-b', label=r'$\phi$ BEM')
+plt.plot(data[2][:nspan - 1, 0], np.degrees(np.resize(data[6], data[2].shape)[:nspan - 1, 0]), '--r',
+         label=r'$\alpha$ LLM')
+plt.plot(data[2][:nspan - 1, 0], np.degrees(np.resize(data[7], data[2].shape)[:nspan - 1, 0]), '--b',
+         label=r'$\phi$ LLM')
 plt.xlabel('r/R (-)')
 plt.ylabel('angle (deg)')
 plt.legend()
@@ -118,11 +122,13 @@ plt.grid(True)
 # Radial distribution F_tan en F_ax
 
 plt.figure()
-plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_Ax, 0), BEM_rR.shape)[0, :]*BEM_rho[0], '-r', label=r'$F_{ax}$ BEM')
-plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_Az, 0), BEM_rR.shape)[0, :]*BEM_rho[0], '-b', label=r'$F_{tan}$ BEM')
+plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_Ax, 0), BEM_rR.shape)[0, :] * BEM_rho[0], '-r', label=r'$F_{ax}$ BEM')
+plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_Az, 0), BEM_rR.shape)[0, :] * BEM_rho[0], '-b', label=r'$F_{tan}$ BEM')
 # Plot one blade of LLM
-plt.plot(data[2][:nspan-1, 0], np.resize(data[3], data[2].shape)[:nspan-1, 0]*BEM_rho[0], '--r', label=r'$F_{ax}$ LLM')
-plt.plot(data[2][:nspan-1, 0], np.resize(data[4], data[2].shape)[:nspan-1, 0]*BEM_rho[0], '--b', label=r'$F_{tan}$ LLM')
+plt.plot(data[2][:nspan - 1, 0], np.resize(data[3], data[2].shape)[:nspan - 1, 0] * BEM_rho[0], '--r',
+         label=r'$F_{ax}$ LLM')
+plt.plot(data[2][:nspan - 1, 0], np.resize(data[4], data[2].shape)[:nspan - 1, 0] * BEM_rho[0], '--b',
+         label=r'$F_{tan}$ LLM')
 plt.xlabel('r/R (-)')
 plt.ylabel('F (N)')
 plt.legend()
@@ -131,11 +137,11 @@ plt.grid(True)
 # Radial distribution circulation
 
 # made non-dimensional with (np.pi * Uinf**2) / (NBlades*Omega)
-circ_nondim = (np.pi*solver.geo.v_inf**2)/(nblades*omega)
+circ_nondim = (np.pi * solver.geo.v_inf ** 2) / (nblades * omega)
 
 plt.figure()
 plt.plot(BEM_rR[0, :], BEM_Gamma[0, :], label=r'$\Gamma$ BEM')
-plt.plot(data[2][:nspan-1, 0], np.resize(data[5], data[2].shape)[:nspan-1, 0]/circ_nondim, label=r'$\Gamma$ LLM')
+plt.plot(data[2][:nspan - 1, 0], np.resize(data[5], data[2].shape)[:nspan - 1, 0] / circ_nondim, label=r'$\Gamma$ LLM')
 plt.xlabel('r/R (-)')
 plt.ylabel(r'$\Gamma$ (-)')
 plt.legend()
@@ -144,9 +150,9 @@ plt.grid(True)
 # INDUCTION FACTORS
 fig, ax = plt.subplots(1, 2, dpi=150)
 ax[0].plot(BEM_rR[0, :], BEM_a[0, :], label="BEM")
-ax[0].plot(data[2][:nspan-1], data[0][:nspan-1], label="Lifting Line")
+ax[0].plot(data[2][:nspan - 1], data[0][:nspan - 1], label="Lifting Line")
 ax[1].plot(BEM_rR[0, :], BEM_aline[0, :], label="BEM")
-ax[1].plot(data[2][:nspan-1], data[1][:nspan-1], label="Lifting Line")
+ax[1].plot(data[2][:nspan - 1], data[1][:nspan - 1], label="Lifting Line")
 ax[0].set_xlabel("r/R [-]")
 ax[0].set_ylabel("a [-]")
 ax[1].set_xlabel("r/R [-]")
@@ -157,7 +163,7 @@ ax[0].legend()
 
 # RADIAL DISTRIBUTION CT
 
-CT_LLM2 = np.resize(data[3], data[2].shape)[:, 0]/(0.5*np.pi*(solver.geo.radius**2)*solver.geo.v_inf**2)
+CT_LLM2 = np.resize(data[3], data[2].shape)[:, 0] / (0.5 * np.pi * (solver.geo.radius ** 2) * solver.geo.v_inf ** 2)
 print('CT Carlos:', np.sum(CT_LLM))
 print('CT:', np.sum(CT_LLM2))
 
--- plotter_double.py
+++ plotter_double.py
@@ -15,7 +15,7 @@ spacing = 'equal'
 nrotor = 2
 
 prop_geo = BladeGeometry(radius=50.0, tsr=8, v_inf=10.0, n_blades=3, n_span=nspan,
-            n_theta=ntheta, spacing=spacing, a=0, xshift=0, yshift=100, zshift=0)
+                         n_theta=ntheta, spacing=spacing, a=0, xshift=0, yshift=100, zshift=0)
 
 blade = prop_geo.bladepanels
 rings = prop_geo.filaments
@@ -23,10 +23,11 @@ solver = LiftingLineSolver(geo=prop_geo, r_rotor=50, weight=0.3, tol=1e-6,
                            n_iter=100, double_rotor=True)
 
 data = solver.run_solver()
-omega = solver.geo.tsr*solver.geo.v_inf/solver.geo.radius
+omega = solver.geo.tsr * solver.geo.v_inf / solver.geo.radius
 [CP_LLM, CT_LLM] = solver.CP_and_CT(np.resize(data[0], data[2].shape), np.resize(data[1], data[2].shape), data[2],
                                     np.resize(data[3], data[2].shape), np.resize(data[4], data[2].shape),
                                     solver.geo.v_inf, omega, solver.geo.radius, nblades)
+
 
 # =============================================================================
 # Double Rotor Plotting
@@ -34,7 +35,7 @@ omega = solver.geo.tsr*solver.geo.v_inf/solver.geo.radius
 
 def plotDoubleRotor():
     prop_geo = BladeGeometry(radius=50.0, tsr=8, v_inf=10.0, n_blades=3, n_span=nspan, n_theta=ntheta,
-                             spacing='cosine', a=0, xshift=0,yshift=100,zshift=0)
+                             spacing='cosine', a=0, xshift=0, yshift=100, zshift=0)
     prop_geo.doubleRotor()
     rings = prop_geo.filaments
 
@@ -48,22 +49,25 @@ def plotDoubleRotor():
     # ax.set_ylim3d(-50, 200)
     # ax.set_zlim3d(-50, 50)
 
-    c=['green','blue','red','green','blue','red']
-    for idx in range(nblades*nrotor):
-        ax.plot_wireframe(rings[0, idx*(nspan-1):(idx+1)*(nspan-1), :ntheta+1],
-                      rings[1, idx*(nspan-1):(idx+1)*(nspan-1), :ntheta+1],
-                      rings[2, idx*(nspan-1):(idx+1)*(nspan-1), :ntheta+1],
-                      color=c[idx], cstride=0)
+    c = ['green', 'blue', 'red', 'green', 'blue', 'red']
+    for idx in range(nblades * nrotor):
+        ax.plot_wireframe(rings[0, idx * (nspan - 1):(idx + 1) * (nspan - 1), :ntheta + 1],
+                          rings[1, idx * (nspan - 1):(idx + 1) * (nspan - 1), :ntheta + 1],
+                          rings[2, idx * (nspan - 1):(idx + 1) * (nspan - 1), :ntheta + 1],
+                          color=c[idx], cstride=0)
+
 
 plotDoubleRotor()
+
 
 # =============================================================================
 # Rotor performance plots
 # =============================================================================
-def plottingFunction(solver,prop_geo,data):
-    plt.plot(prop_geo.centerPoints,data[-1][:len(prop_geo.centerPoints)])
+def plottingFunction(solver, prop_geo, data):
+    plt.plot(prop_geo.centerPoints, data[-1][:len(prop_geo.centerPoints)])
     plt.show()
     return
+
 
 # plottingFunction(solver,prop_geo,data)
 
@@ -72,20 +76,20 @@ def plottingFunction(solver,prop_geo,data):
 # =============================================================================
 plt.close('All')
 
-[BEM_rR, BEM_alpha, BEM_phi, BEM_rho, BEM_Ax, BEM_Az, BEM_Gamma , BEM_CT, BEM_CP, BEM_a, BEM_aline] = read_matlab_data()
+[BEM_rR, BEM_alpha, BEM_phi, BEM_rho, BEM_Ax, BEM_Az, BEM_Gamma, BEM_CT, BEM_CP, BEM_a, BEM_aline] = read_matlab_data()
 
 # Radial distribution alpha and phi
 
 plt.figure(dpi=150)
-plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_alpha, 0), BEM_rR.shape)[0, :]*180/np.pi, '-r', label=r'$\alpha$ BEM')
-plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_phi, 0), BEM_rR.shape)[0, :]*180/np.pi, '-b', label=r'$\phi$ BEM')
-plt.plot(data[2][:nspan-1, 0], np.degrees(np.resize(data[6], data[2].shape)[:nspan-1, 0]), '--r',
+plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_alpha, 0), BEM_rR.shape)[0, :] * 180 / np.pi, '-r', label=r'$\alpha$ BEM')
+plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_phi, 0), BEM_rR.shape)[0, :] * 180 / np.pi, '-b', label=r'$\phi$ BEM')
+plt.plot(data[2][:nspan - 1, 0], np.degrees(np.resize(data[6], data[2].shape)[:nspan - 1, 0]), '--r',
          label=r'$\alpha$ 'r'LLM - rotor 1')
-plt.plot(data[2][:nspan-1, 0], np.degrees(np.resize(data[7], data[2].shape)[:nspan-1, 0]), '--b',
+plt.plot(data[2][:nspan - 1, 0], np.degrees(np.resize(data[7], data[2].shape)[:nspan - 1, 0]), '--b',
          label=r'$\phi$ 'r'LLM - rotor 1')
-plt.plot(data[2][-(nspan-1):, 0], np.degrees(np.resize(data[6], data[2].shape)[-(nspan-1):, 0]), ':r',
+plt.plot(data[2][-(nspan - 1):, 0], np.degrees(np.resize(data[6], data[2].shape)[-(nspan - 1):, 0]), ':r',
          label=r'$\alpha$ LLM - rotor 2')
-plt.plot(data[2][-(nspan-1):, 0], np.degrees(np.resize(data[7], data[2].shape)[-(nspan-1):, 0]), ':b',
+plt.plot(data[2][-(nspan - 1):, 0], np.degrees(np.resize(data[7], data[2].shape)[-(nspan - 1):, 0]), ':b',
          label=r'$\phi$ LLM - rotor 2')
 plt.xlabel('r/R (-)')
 plt.ylabel('angle (deg)')
@@ -95,16 +99,16 @@ plt.grid(True)
 # Radial distribution F_tan en F_ax
 
 plt.figure(dpi=150)
-plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_Ax, 0), BEM_rR.shape)[0, :]*BEM_rho[0], '-r', label=r'$F_{ax}$ BEM')
-plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_Az, 0), BEM_rR.shape)[0, :]*BEM_rho[0], '-b', label=r'$F_{tan}$ BEM')
+plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_Ax, 0), BEM_rR.shape)[0, :] * BEM_rho[0], '-r', label=r'$F_{ax}$ BEM')
+plt.plot(BEM_rR[0, :], np.resize(np.mean(BEM_Az, 0), BEM_rR.shape)[0, :] * BEM_rho[0], '-b', label=r'$F_{tan}$ BEM')
 # Plot one blade of LLM
-plt.plot(data[2][:nspan-1, 0], np.resize(data[3], data[2].shape)[:nspan-1, 0]*BEM_rho[0], '--r',
+plt.plot(data[2][:nspan - 1, 0], np.resize(data[3], data[2].shape)[:nspan - 1, 0] * BEM_rho[0], '--r',
          label=r'$F_{ax}$ LLM - rotor 1')
-plt.plot(data[2][:nspan-1, 0], np.resize(data[4], data[2].shape)[:nspan-1, 0]*BEM_rho[0], '--b',
+plt.plot(data[2][:nspan - 1, 0], np.resize(data[4], data[2].shape)[:nspan - 1, 0] * BEM_rho[0], '--b',
          label=r'$F_{'r'tan}$ LLM - rotor 1')
-plt.plot(data[2][-(nspan-1):, 0], np.resize(data[3], data[2].shape)[-(nspan-1):, 0]*BEM_rho[0], ':r',
+plt.plot(data[2][-(nspan - 1):, 0], np.resize(data[3], data[2].shape)[-(nspan - 1):, 0] * BEM_rho[0], ':r',
          label=r'$F_{'r'ax}$ LLM - rotor 2')
-plt.plot(data[2][-(nspan-1):, 0], np.resize(data[4], data[2].shape)[-(nspan-1):, 0]*BEM_rho[0], ':b',
+plt.plot(data[2][-(nspan - 1):, 0], np.resize(data[4], data[2].shape)[-(nspan - 1):, 0] * BEM_rho[0], ':b',
          label=r'$F_{'r'tan}$ LLM - rotor 2')
 plt.xlabel('r/R (-)')
 plt.ylabel('F (N)')
@@ -114,12 +118,13 @@ plt.grid(True)
 # Radial distribution circulation
 
 # made non-dimensional with (np.pi * Uinf**2) / (NBlades*Omega)
-circ_nondim = (np.pi*solver.geo.v_inf**2)/(nblades*omega)
+circ_nondim = (np.pi * solver.geo.v_inf ** 2) / (nblades * omega)
 
 plt.figure(dpi=150)
 plt.plot(BEM_rR[0, :], BEM_Gamma[0, :], label=r'$\Gamma$ BEM')
-plt.plot(data[2][:nspan-1, 0], np.resize(data[5], data[2].shape)[:nspan-1, 0]/circ_nondim, label=r'$LLM - rotor 1$')
-plt.plot(data[2][-(nspan-1):, 0], np.resize(data[5], data[2].shape)[-(nspan-1):, 0]/circ_nondim,
+plt.plot(data[2][:nspan - 1, 0], np.resize(data[5], data[2].shape)[:nspan - 1, 0] / circ_nondim,
+         label=r'$LLM - rotor 1$')
+plt.plot(data[2][-(nspan - 1):, 0], np.resize(data[5], data[2].shape)[-(nspan - 1):, 0] / circ_nondim,
          label=r'$LLM - rotor 2$')
 plt.xlabel('r/R (-)')
 plt.ylabel(r'$\Gamma$ (-)')
@@ -130,11 +135,11 @@ plt.grid(True)
 
 # INDUCTION FACTORS
 fig, ax = plt.subplots(1, 2, dpi=150)
-ax[0].plot(data[2][:nspan-1], data[0][:nspan-1], label="LL - rotor 1")
-ax[0].plot(data[2][-(nspan-1):], data[0][-(nspan-1):], label="LL - rotor 2")
+ax[0].plot(data[2][:nspan - 1], data[0][:nspan - 1], label="LL - rotor 1")
+ax[0].plot(data[2][-(nspan - 1):], data[0][-(nspan - 1):], label="LL - rotor 2")
 ax[0].plot(BEM_rR.flatten(), BEM_a[0, :], label="BEM (single rotor)")
-ax[1].plot(data[2][:nspan-1], data[1][:nspan-1])
-ax[1].plot(data[2][-(nspan-1):], data[1][-(nspan-1):])
+ax[1].plot(data[2][:nspan - 1], data[1][:nspan - 1])
+ax[1].plot(data[2][-(nspan - 1):], data[1][-(nspan - 1):])
 ax[1].plot(BEM_rR.flatten(), BEM_aline[0, :])
 ax[0].set_xlabel("r/R [-]")
 ax[0].set_ylabel("a [-]")
